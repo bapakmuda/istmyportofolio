73b447a7d83e66999f5ae4183192c4749d06dfd4
RYANFIRD <fird@RYANFIRDs-MacBook-Pro.local>
2026-09-24 12:20:37 +0700

    feat: prebuild Next.js locally and bypass hostinger build

---
+++ .next/server/chunks/ssr/_next-internal_server_app_kamar-belakang_page_actions_1s643nf.js
@@ -0,0 +1,3 @@
+module.exports=[76249,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_kamar-belakang_page_actions_1s643nf.js.map
---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,25 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/0cz1d0mv5g_q7.js"
+  ],
+  "lowPriorityFiles": [
+    "static/GtlwXIt5_ROHdc6lG7cCh/_buildManifest.js",
+    "static/GtlwXIt5_ROHdc6lG7cCh/_ssgManifest.js",
+    "static/GtlwXIt5_ROHdc6lG7cCh/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/3yfl9fgvivx3-.js",
+    "static/chunks/0fy4h0ngk1igg.js",
+    "static/chunks/2w6-remy08ye_.js",
+    "static/chunks/196x_tfexoucg.js",
+    "static/chunks/11jq0c2_zavac.js",
+    "static/chunks/turbopack-27_puapyh985m.js"
+  ],
+  "rootMainFilesTree": {},
+  "pagesChunkGroupBootstrapParams": {},
+  "chunkLoadingGlobal": "TURBOPACK"
+};
